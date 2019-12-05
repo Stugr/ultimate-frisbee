c862252aef0360ff3d5dd1a4cddbb439bf7ee5cc
stugr <nick@stugr.com>
2019-12-06 00:43:04 +1100

    teamsize change

--- MelbourneHat/Group-PeopleIntoTeams.ps1
+++ MelbourneHat/Group-PeopleIntoTeams.ps1
@@ -1,7 +1,7 @@
 $people = Import-Csv "$PSScriptRoot\Hat Rego 2nd december.csv"
 
 # set team size
-$teamSize = 13
+$teamSize = 12
 
 # how many people in the csv
 "There are $($people.count) in the csv"
